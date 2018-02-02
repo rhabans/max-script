aa272746fd67aa048c040a05bc56e16737a006a7
rhabans <rhabans@gmail.com>
2018-02-02 11:03:25 -0600

    adding in comments to shell script for verbosity

--- wordcount.sh
+++ wordcount.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# get rid of comment lines
+# search for word (case insensitive, whole words only)
+# translate spaces to newlines
+# search for word (case insensitive, whole words only)
+# word count by line to get total number of occurences
 grep -v "^#" $2 | grep -i -w $1 | tr ' ' '\n' | grep -i -w $1 | wc -l
